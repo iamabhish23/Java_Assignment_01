7b5ef097e12a3cd4642f8488318345ac647d519b
Abhishek tiwari <iamabhishek.tiwari23@gmail.com>
2023-09-17 16:49:12 +0530

    done

--- CountNotes.java
+++ CountNotes.java
@@ -3,43 +3,24 @@ import java.util.Scanner;
 public class CountNotes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the amount in rupees: ");
         int amount = scanner.nextInt();
+        int totalNotes = 0;
 
-        int notes500 = amount / 500;
-        int remainingAmount500 = amount % 500;
+        int[] notes = {500, 200, 100, 50, 20, 10, 5, 2, 1};
 
-        int notes200 = remainingAmount500 / 200;
-        int remainingAmount200 = remainingAmount500 % 200;
+        for (int i = 0; i < notes.length; i++) {
+            int count = amount / notes[i];
+            amount %= notes[i];
+            totalNotes+=count;
+            if(count!=0){
+                System.out.println("Number of " + notes[i] + " rupee notes: " + count);
+            }
+            
+        }
 
-        int notes100 = remainingAmount200 / 100;
-        int remainingAmount100 = remainingAmount200 % 100;
+        System.out.println("Total Number of Notes are: " + totalNotes);
 
-        int notes50 = remainingAmount100 / 50;
-        int remainingAmount50 = remainingAmount100 % 50;
-
-        int notes20 = remainingAmount50 / 20;
-        int remainingAmount20 = remainingAmount50 % 20;
-
-        int notes10 = remainingAmount20 / 10;
-        int remainingAmount10 = remainingAmount20 % 10;
-
-        int notes5 = remainingAmount10 / 5;
-        int remainingAmount5 = remainingAmount10 % 5;
-
-        int notes2 = remainingAmount5 / 2;
-        int notes1 = remainingAmount5 % 2;
-
-        System.out.println("Number of 500 rupee notes: " + notes500);
-        System.out.println("Number of 200 rupee notes: " + notes200);
-        System.out.println("Number of 100 rupee notes: " + notes100);
-        System.out.println("Number of 50 rupee notes: " + notes50);
-        System.out.println("Number of 20 rupee notes: " + notes20);
-        System.out.println("Number of 10 rupee notes: " + notes10);
-        System.out.println("Number of 5 rupee coins: " + notes5);
-        System.out.println("Number of 2 rupee coins: " + notes2);
-        System.out.println("Number of 1 rupee coins: " + notes1);
         scanner.close();
     }
 }
